206af4e249eeefdd04af9fe4443a6133da67e512
William Ferreira <william.luis.ferreira@usp.br>
2019-10-01 16:52:43 -0300

    Hello

---
+++ src/Componentes/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/**
+ * @param {Null} Null  Null
+ * @description Classe geradora da função app
+ * @constructor
+ */
+
+class App extends React.Component {
+    render() {
+      return <h1>Hello</h1>;
+    }
+  }
+
+  export default App
